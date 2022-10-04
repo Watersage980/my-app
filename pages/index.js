24fae5dc6465fe7bd152ba88f57ea93be631bc80
Ezequiel <ezequielsennesantos@gmail.com>
2022-10-04 10:08:07 -0300

    ko

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1> Simbora <br>yoso yoso vs yami yami</br></h1>
+    <h1> Simbora yoso yoso vs yami yami</h1>
   )
 }
